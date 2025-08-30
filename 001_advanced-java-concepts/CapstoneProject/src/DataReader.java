59c071d5d5221a1d463a63627c01319a93bae1f0
ravihamidov <ravihamidov42@gmail.com>
2025-08-31 02:27:50 +0400

    fix: GitHub Organize

---
+++ 001_advanced-java-concepts/CapstoneProject/src/DataReader.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataReader {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+    public List<Transaction> readTransaction(String filePath) {
+        List<Transaction> transactions = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))){
+            reader.readLine();
+
+            String line;
+            while((line = reader.readLine())!=null){
+                    String[] data = line.split(",");
+                if (data.length == 5) {
+                    String transactionId = data[0];
+                    String userId = data[1];
+                    double amount = Double.parseDouble(data[2]);
+                    LocalDateTime timestamp = LocalDateTime.parse(data[3], formatter);
+                    String location = data[4];
+
+                    transactions.add(new Transaction(transactionId, userId, amount, timestamp, location));
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("XETA: Fayl tapilmadi: " + filePath);
+        } catch (IOException e) {
+            System.err.println("XETA: Faylı oxuyarken problem yarandi: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("XƏTA: Məlumatları emal edərkən problem yarandı: " + e.getMessage());
+        }
+
+        return transactions;
+    }
+}
